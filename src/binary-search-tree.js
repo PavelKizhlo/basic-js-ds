d65af659ea5ab129ea62730b659d322a295c92bf
Pavel Kizhlo <pavelkizh@gmail.com>
2022-05-02 19:31:19 +0300

    feat: complete binary-search-tree task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -38,23 +38,107 @@ class BinarySearchTree {
   }
 
   has(data) {
+    return searchData(this.rootOfTree, data);
 
+    function searchData(node, data) {
+      if (!node) {
+        return false;
+      }
+
+      if (node.data === data) {
+        return true;
+      }
+
+      if (node.data > data) {
+        return searchData(node.left, data);
+      } else {
+        return searchData(node.right, data);
+      }
+    }
   }
 
   find(data) {
+    return findData(this.rootOfTree, data);
 
+    function findData(node, data) {
+      if (!node) {
+        return null;
+      }
+
+      if (node.data === data) {
+        return node;
+      }
+
+      if (node.data > data) {
+        return findData(node.left, data);
+      } else {
+        return findData(node.right, data);
+      }
+    }
   }
 
   remove(data) {
+    this.rootOfTree = removeNode(this.rootOfTree, data);
 
+    function removeNode(node, data) {
+      if (!node) {
+        return null;
+      }
+
+      if (node.data > data) {
+        node.left = removeNode(node.left, data);
+        return node;
+      } else if (node.data < data) {
+        node.right = removeNode(node.right, data);
+        return node;
+      } else {
+        if (!node.left && !node.right) {
+          return null;
+        }
+
+        if (!node.left) {
+          node = node.right;
+          return node;
+        }
+
+        if (!node.right) {
+          node = node.left;
+          return node;
+        }
+
+        let minFromRight = node.right;
+
+        while (minFromRight.left) {
+          minFromRight = minFromRight.left;
+        }
+
+        node.data = minFromRight.data;
+
+        node.right = removeNode(node.right, minFromRight.data);
+
+        return node;
+      }
+    }
   }
 
   min() {
+    let min = this.rootOfTree;
 
+    while (min.left) {
+      min = min.left;
+    }
+
+    return min.data;
   }
 
   max() {
+    let max = this.rootOfTree;
 
+    while (max.right) {
+      max = max.right;
+    }
+
+    return max.data;
   }
 }
 
@@ -62,11 +146,3 @@ module.exports = {
   BinarySearchTree
 };
 
-// let bst = new BinarySearchTree();
-// bst.add(1);
-// bst.add(3);
-// bst.add(2);
-// bst.add(5);
-// bst.add(7);
-
-// console.log(bst.root.right)
